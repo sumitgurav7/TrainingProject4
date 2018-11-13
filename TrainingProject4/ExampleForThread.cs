236227f520ef70d2800fb9b8338232d35d3a5dc1
Sumit Gurav <sumitkiran.gurav@euromonitor.com>
2018-11-13 16:06:10 +0530

    Test

---
+++ TrainingProject4/ExampleForThread.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TrainingProject4
+{
+    class ExampleForThread
+    {
+        public static void StartThisProcess()
+        {
+            for(int i =0; i<10; i++)
+            {
+                Console.WriteLine("Inside Thread Example " + i);
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
--- TrainingProject4/Program.cs
+++ TrainingProject4/Program.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace TrainingProject4
 {
+    /// <summary>
+    /// this is for threading Poc
+    /// 
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("this is main program");
+
+            Thread t = new Thread(new ThreadStart(ExampleForThread.StartThisProcess));
+
+            t.Start();
+
+            for(int i =0; i<6; i++)
+            {
+                Console.WriteLine("this is main thread after starting t thread");
+                Thread.Sleep(0);
+            }
+            t.Join();
+            Console.ReadKey();
         }
     }
 }
